59def7269ea268c6c512d1cb23966eeac369e665
brandonespino <brandoneh122@gmail.com>
2022-03-22 13:44:53 -0700

    fixed frame parser

--- frame_parser.py
+++ frame_parser.py
@@ -1,13 +1,16 @@
-# import cv2 as cv
+import cv2 as cv
 
-# capture = cv.VideoCapture('TelloVideo/afternoon.mp4')
-# skipped = None # only save every skipped (e.g. 80th) frame
-# frame_num = 0
-# file_name_num = 0
-# while(True):
-#     success, frame = capture.read()
-#         if success:
-#             if file_name_num%skipped == 0:
-#                 _cv.({}.jpg'.format(file_name_num), frame) #save the image 
-#         else:
-#             break�/T += 1g�.release()
+capture = cv.VideoCapture('TelloVideo/afternoon.mp4')
+frames_to_skip = None # number of frames to skip between saves
+frame_num = 0
+file_name_num = 0
+while(True):
+    success, frame = capture.read()
+    if success:
+        if frame_num%frames_to_skip == 0:
+            cv.imwrite('afternoon_{}.jpg'.format(file_name_num), frame) # save the image 
+            file_name_num+=1
+    else:
+        break
+    frame_num+=1
+capture.release()
