5a5c60323942e8044a6d12d6e7282522e51e0a64
brandonespino <brandoneh122@gmail.com>
2022-03-21 23:39:32 -0700

    files mostly recovered

--- frame_parser.py
+++ frame_parser.py
@@ -1,13 +1,13 @@
-import cv2 as cv
+# import cv2 as cv
 
-capture = cv.VideoCapture('TelloVideo/afternoon.mp4')
-skipped = None # only save every skipped (e.g. 80th) frame
-frame_num = 0
-file_name_num = 0
-while(True):
-    success, frame = capture.read()
-        if success:
-            if file_name_num%skipped == 0:
-                _cv.({}.jpg'.format(file_name_num), frame) #save the image 
-        else:
-            break�/T += 1g�.release()
+# capture = cv.VideoCapture('TelloVideo/afternoon.mp4')
+# skipped = None # only save every skipped (e.g. 80th) frame
+# frame_num = 0
+# file_name_num = 0
+# while(True):
+#     success, frame = capture.read()
+#         if success:
+#             if file_name_num%skipped == 0:
+#                 _cv.({}.jpg'.format(file_name_num), frame) #save the image 
+#         else:
+#             break�/T += 1g�.release()
